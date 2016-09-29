f020b6cc350b3133cfd3d049dbf482e75d910a23
Barret Rennie <barret@brennie.ca>
2016-09-28 19:52:11 -0600

    Use PostCSS for css processing

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './style';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<span>Welcome to FlyRTC</span>, document.getElementById('content'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 const config = {
@@ -28,6 +29,13 @@ const config = {
           cacheDirectory: true,
         },
       },
+      {
+        test: /\.pcss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          '!css-loader?sourceMap&importLoaders=1!postcss-loader'
+        ),
+      }
     ],
   },
 
@@ -37,11 +45,34 @@ const config = {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       },
     }),
+    new ExtractTextPlugin(path.join('static', 'app.min.css')),
   ],
 
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['', '.pcss', '.js'],
   },
+
+  postcss: webpack => ({
+    defaults: [
+      require('stylelint'),
+      require('postcss-import')({
+        addDependencyTo: webpack,
+        path: [
+          path.join(__dirname, 'node_modules'),
+        ],
+      }),
+      require('postcss-simple-vars'),
+      require('postcss-color-function'),
+      require('postcss-nested'),
+      require('colorguard'),
+      require('autoprefixer')({browsers: 'Firefox >= 52'}),
+      require('cssnano'),
+      require('postcss-reporter')({
+        clearMessages: true,
+        throwError: true,
+      }),
+    ],
+  }),
 };
 
 if (process.env.NODE_ENV === 'production') {
